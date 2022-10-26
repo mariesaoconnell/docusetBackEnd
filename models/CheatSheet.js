8961d7a3d53d9af1f91ece42479ae18696ce7e1f
Zac Marcengill <zacmarcengill@gmail.com>
2022-10-25 23:09:10 -0500

    Updated CheatSheet.js with body: {}

--- models/CheatSheet.js
+++ models/CheatSheet.js
@@ -3,7 +3,7 @@ const mongoose = require('../db/connection')
 const CheatSheetSchema = new mongoose.Schema({
 	title: {type: String, required: true},
 	subject: {type: String, required: true},
-  body: {type: String, required: true}
+  body: {}
 });
 
 const CheatSheet = mongoose.model('CheatSheet', CheatSheetSchema);
